f9930f4cbb090253c49ab3d66cc880da934bcd67
AndreMayerT <andremayerusa@gmail.com>
2022-10-13 23:28:24 -0300

    Initial Commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let data = {}
+var profileImage = document.querySelector('.profileMain img')
+var profileName = document.querySelector('.profileMain h2')
+
+fetch('https://api.github.com/users/AndreMayert')
+.then(response => response.json())
+.then(myData => {
+    data = myData
+
+    profileImage.src = data.avatar_url
+    profileName.textContent = data.name
+    console.log(data)
+})
