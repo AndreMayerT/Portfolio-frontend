d2ce1b6d174f4e95cadb7d6a9a30cac2b0972405
AndreMayerT <andremayerusa@gmail.com>
2022-10-15 15:18:21 -0300

    add js script

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let data = {}
 var profileImage = document.querySelector('.profileMain img')
 var profileName = document.querySelector('.profileMain h2')
 
-fetch('https://api.github.com/users/AndreMayert')
+fetch('https://api.github.com/users/AndreMayert/repos')
 .then(response => response.json())
 .then(myData => {
     data = myData
